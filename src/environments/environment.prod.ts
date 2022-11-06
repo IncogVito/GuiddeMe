13f83d8a1e108f26584a4d276711affa622167dc
Witold Drożdżowski <witold.drozdzowskir@gmail.com>
2022-11-06 19:09:05 +0100

    Fixing Firebase deploy v2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
+const firebaseConfig = {
+  apiKey: "*****",
+  authDomain: "guidde-me.firebaseapp.com",
+  projectId: "guidde-me",
+  storageBucket: "guidde-me.appspot.com",
+  messagingSenderId: "*****",
+  appId: "*****"
+};
+
 export const environment = {
   production: true,
+  firebase: firebaseConfig,
   emulator: false
 };
